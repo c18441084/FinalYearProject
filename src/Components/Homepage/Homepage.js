8e44a3db29f9174f10ef19d9debdd698e10cfe78
c18441084 <c18441084@mytudublin.ie>
2021-12-20 19:55:28 +0000

    Adding more updates

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,33 @@
+//import react from "react";
+import './Homepage.css'
+
+export default function Homepage(){
+
+    return(
+        <div>
+            <title>FindMyOwner</title>
+            <div id = "Title">
+                <h1>FindMyOwner</h1>
+            </div>
+            <hr style={{backgroundColor: "black"}}></hr>
+            <div id = "sidebar">
+                <a href = "#">
+                    <h2>Found Pets</h2>
+                </a>
+                <a href = "#">
+                    <h2>Lost Pets</h2>
+                </a>
+                <a href = "#">
+                    <h2>About Us</h2>
+                </a>
+                <a href = "#">
+                    <h2 id = "DWS">Dog Warden Service</h2>
+                </a>
+            </div>
+            <div id = "topbar">
+                <h3 id = "RP">Recent Posts</h3>
+                <h3 id = "PNM">Posts near me</h3>
+            </div>
+        </div>
+    )
+}
