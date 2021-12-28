45c602b314b87feaa1b96e6a98c84f5b15fcc65d
c18441084 <c18441084@mytudublin.ie>
2021-12-28 17:57:10 +0000

    Add

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Homepage from './Components/Homepage/Homepage';
 import Found from './Components/Found/Found';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-
 
 function App() {
-
   return (
-   <div className="App">
-    <BrowserRouter>
-      <Routes>
-        <Route path = "/" element = {<Homepage />} />
-        <Route exact path = "/Found" element = {<Found />} />
-      </Routes>
-    </BrowserRouter>
-  </div>
-
-
+    <div className="App">
+      <Homepage></Homepage>
+    </div>
   );
 }
 
--- src/Components/Found/Found.js
+++ src/Components/Found/Found.js
@@ -4,7 +4,7 @@ export default function Found(){
 
     return(
         <div>
-            <h1>Hello</h1>
+            
         </div>
     )
 }
