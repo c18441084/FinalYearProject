9d4a149351577f498605a099e948dc0e47c9ecdc
c18441084 <c18441084@mytudublin.ie>
2021-12-28 16:31:06 +0000

    Adding Firebase

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Homepage from './Components/Homepage/Homepage';
 import Found from './Components/Found/Found';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <Homepage></Homepage>
-    </div>
+   <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Homepage />} />
+        <Route exact path = "/Found" element = {<Found />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+
+
   );
 }
 
