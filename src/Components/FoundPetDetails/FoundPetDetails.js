f6b466aa6e6a8a4a679796c5bc87919aa0e436d7
c18441084 <c18441084@mytudublin.ie>
2022-01-05 15:45:03 +0000

    Adding in breedhelper

--- src/Components/FoundPetDetails/FoundPetDetails.js
+++ src/Components/FoundPetDetails/FoundPetDetails.js
@@ -6,11 +6,16 @@ export default function FoundPetDetails(){
 
     const [type, setType] = useState("");
     const [dogBreed, setDogBreed] = useState("");
+    const [dogBreedHelp, setDogBreedHelp] = useState("");
 
     const [showBreed, setShowBreed] = useState(false);
+    const [showBreedGuide, setShowBreedGuide] = useState(false);
+    const [showBreedHelpImages, setShowBreedHelpImages] = useState(false);
     const [showHeight, setShowHeight] = useState(false);
+    const [showHeightGuide, setShowHeightGuide] = useState(false);
 
     const [breedList, setBreedList] = useState([]);
+    const [breedListImages, setBreedListImages] = useState([]);
 
     async function componentDidMount(){
         const response = await fetch("https://dog.ceo/api/breeds/list/all");
@@ -18,9 +23,18 @@ export default function FoundPetDetails(){
         setBreedList(data.message);
     }
 
+    async function DogImages(){
+        const response = await fetch("https://dog.ceo/api/breed/" + dogBreedHelp + "/images/random/3");
+        const data = await response.json();
+        setBreedListImages(data.message);
+        setShowBreedHelpImages(true);
+    }
+
     function Type(){
         console.log(type);
         setShowBreed(false);
+        setShowBreedGuide(false);
+        setShowBreedHelpImages(false);
         setShowHeight(false);
         if(type == "dog"){
             setShowBreed(true);
@@ -35,8 +49,22 @@ export default function FoundPetDetails(){
         setShowHeight(true);
     }
 
+    function BreedHelp(){
+        setShowHeightGuide(false);
+        setShowBreedGuide(true);
+    }
+
+    function closeBreedHelp(){
+        setShowBreedGuide(false);
+        setShowBreedHelpImages(false);
+    }
+
     function heightGuide(){
-        alert("Measure from the front foot of the animal to the top of the head." + heightDiagram);
+        setShowHeightGuide(true);
+    }
+
+    function closeHeightGuide(){
+        setShowHeightGuide(false);
     }
 
 
@@ -65,8 +93,33 @@ export default function FoundPetDetails(){
                             )
                         })}
                     </select>
-                    <button>Need Help?</button>
+                    <button id = "breedHelp" onClick={BreedHelp}>Need Help?</button>
                     <br/>
+                </div>
+            : null}
+            {showBreedGuide?
+                <div id = "dogBreedHelp">
+                    <button id="closeBreedGuideButton" onClick={closeBreedHelp}>X</button>
+                    <br/>
+                    <label for="DbreedHelp">Select a Dog Breed to view image: </label>
+                    <select name="dogimage" id="DbreedHelp" onChange={DogImages} onInput= {(breedhelp) => setDogBreedHelp(breedhelp.target.value)}>
+                        <option value="" disabled selected hidden>Select a Dog Breed</option>
+                        {Object.keys(breedList).map(function (element){
+                            return (
+                                <option>{element}</option>
+                            )
+                        })}
+                    </select>
+                    {showBreedHelpImages? 
+                        <div>
+                            {breedListImages.map(function (element){
+                                console.log("in");
+                                return(
+                                    <img src={element} id="breedImage"></img>
+                                )
+                            })}
+                        </div>
+                    : null}
                 </div>
             : null}
 
@@ -74,9 +127,16 @@ export default function FoundPetDetails(){
                 <div id="height">
                     <label for="heightInput">Height: </label>
                     <input id="heightInput" type="number" min = "0" max = "200"/>
-                    <p>cm</p>
+                    <p id="centimetres">cm</p>
                     <button id="heightHelp" onClick={heightGuide}>?</button>
                 </div> 
+            : null}
+            {showHeightGuide?
+                <div id="height_guide">
+                    <button id="closeHeightGuideButton" onClick={closeHeightGuide}>X</button>
+                    <h3>Measure from the front foot of the animal to the top of the head.</h3>
+                    <img id="heightGuideImage" src={heightDiagram}/>
+                </div>
             : null}
         </div>
     )
