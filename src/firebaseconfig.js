31733dc0d1aa3bb972695af1a82680605e1f6992
c18441084 <c18441084@mytudublin.ie>
2022-01-05 00:00:22 +0000

    Unnecessary Deletes

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -1,7 +1,7 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import { signInWithPopup, FacebookAuthProvider, signOut } from "firebase/auth";
+import { signInWithPopup, FacebookAuthProvider} from "firebase/auth";
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
