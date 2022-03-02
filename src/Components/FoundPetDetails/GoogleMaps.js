da994b9f373a5d0a5371e8fc2239543bb5045c0e
c18441084 <c18441084@mytudublin.ie>
2022-03-02 21:42:25 +0000

    Comments Working

--- src/Components/FoundPetDetails/GoogleMaps.js
+++ src/Components/FoundPetDetails/GoogleMaps.js
@@ -3,38 +3,39 @@ import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 import { googleAPIkey, geocodeAPIkey } from '../../keys';
 
 const style = {
-    maxWidth: "75%",
-    height: "75%",
+    maxWidth: "57%",
+    height: "57%",
     overflowX: "hidden",
-    overflowY: "hidden"
+    overflowY: "hidden",
+    marginLeft: "1.6%",
+    marginBottom: "40%",
 };
 
-const address="";
-
 export class MapContainer extends Component {
-    state = {
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {},
-      address: " ",
-      mapCenter: {
-        lat: 53.350140,
-        lng: -6.266155
-      }
-    };
+    
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+    address: " ",
+    mapCenter: {
+      lat: 53.350140,
+      lng: -6.266155
+    }
+  };
 
-    onMapClicked = async (props, marker, e) => {
-      const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${e.latLng.lat()},${e.latLng.lng()}&key=${geocodeAPIkey}`);
-      const addressObject = await response.json();
-      const gettingAddress = addressObject.results[0].formatted_address
-      //address = gettingAddress;
-      console.log(gettingAddress);
-      this.setState({
-        selectedPlace: props,
-        mapCenter: {lat: e.latLng.lat(), lng: e.latLng.lng()},
-        address: gettingAddress,
-      })
-    };
+  onMapClicked = async (props, marker, e) => {
+    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${e.latLng.lat()},${e.latLng.lng()}&key=${geocodeAPIkey}`);
+    const addressObject = await response.json();
+    const gettingAddress = addressObject.results[0].formatted_address
+    //address = gettingAddress;
+    console.log(gettingAddress);
+    this.setState({
+      selectedPlace: props,
+      mapCenter: {lat: e.latLng.lat(), lng: e.latLng.lng()},
+      address: gettingAddress,
+    })
+  };
     
   render() {
     return (
